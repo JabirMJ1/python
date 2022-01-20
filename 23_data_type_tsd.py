875879db4947f0858a37d050d875694bf5bdcdd1
JabirMJ1 <jabirmj1@gmail.com>
2022-01-20 15:57:13 +0530

    lambda

---
+++ 23_data_type_tsd.py
@@ -0,0 +1,21 @@
+# Check if a number is neon or not
+
+def check_neon(number):
+    temp = number
+    temp = temp**2
+    sum = 0
+    while(temp>0):
+        last = temp % 10
+        sum = sum + last
+        temp = temp // 10
+    if(sum == number):
+        return True
+    else:
+        return False
+
+n = int(input("Enter the number: "))
+
+if(check_neon(n)):
+    print(f"{n} is a neon number")
+else:
+    print(f"{n} is not a neon number")
--- extra/palindrome.py
+++ extra/palindrome.py
@@ -7,8 +7,8 @@ reverse = 0
 while n>0:
     last_digit = n % 10
     reverse = (reverse * 10) + last_digit
-    n = n / 10
-
+    n = n // 10
+    
 if reverse==backup:
     print(f"{backup} is a palindrome")
 else:
